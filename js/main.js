8a44c1a5fa78936a92f5bf849ef9f0117f9316c2
Johan Castillo <jcjohan2707@gmail.com>
2020-08-21 09:20:09 -0400

    v-model, metodos y evento click

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ const app = new Vue({
       {nombre: 'Manzana', cantidad: 0},
       {nombre: 'Cambur', cantidad: 20},
       {nombre: 'Fresa', cantidad: 50},
-    ]
+    ],
+    nuevaFruta: ''
+  },
+  methods: {
+    agregarFruta () {
+      this.frutas.push({
+        nombre: this.nuevaFruta, cantidad: 0
+      })
+    }
   }
-});
+})
