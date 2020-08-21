0f00b9d2e6e280abf4698534e6dc5bcc408e8447
Johan Castillo <jcjohan2707@gmail.com>
2020-08-20 20:07:02 -0400

    trabajando con v-for

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'Hola Mundo con Vue'
+    title: 'Hola Mundo con Vue',
+    frutas: [
+      {nombre: 'Pera', cantidad: 10},
+      {nombre: 'Manzana', cantidad: 5},
+      {nombre: 'Cambur', cantidad: 20},
+      {nombre: 'Fresa', cantidad: 50},
+    ]
   }
 });
