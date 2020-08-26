d9a6ae6b40085642dfd9f9f553add52e4c4567ff
Johan Castillo <jcjohan2707@gmail.com>
2020-08-25 22:26:04 -0400

    Propiedades Computadas

---
+++ project/project/js/main.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Hola Mundo con Vue',
+    fondo: 'bg-warning',
+    color: false,
+    frutas: [
+      {nombre: 'Pera', cantidad: 10},
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Cambur', cantidad: 20},
+      {nombre: 'Fresa', cantidad: 50},
+    ],
+    nuevaFruta: '',
+    total: 0,
+  },
+  methods: {
+    agregarFruta () {
+      this.frutas.push({
+        nombre: this.nuevaFruta, cantidad: 0
+      });
+      this.nuevaFruta = "";
+    }
+  },
+  computed: {
+    sumarFrutas(){
+      this.total = 0;
+      for(fruta of this.frutas){
+        this.total = this.total + fruta.cantidad;
+      }
+      return this.total;
+    }
+  }
+})
