1126a86d4e09f49294dca973088464ba52240a9e
Johan Castillo <jcjohyan2707@gmail.com>
2020-08-26 10:45:20 -0400

    Ejemplo con propiedades computadas en Vue

--- computed/js/app.js
+++ computed/js/app.js
@@ -2,5 +2,10 @@ const app = new Vue({
   el: '#app',
   data: {
     mensaje: "Hola mundo con Vue"
+  },
+  computed: {
+    invertido(){
+      return this.mensaje.split('').reverse().join('');
+    }
   }
 })
