a020bf41649dd327681dd966e7ed50334c07ae58
Johan Castillo <jcjohyan2707@gmail.com>
2020-08-26 13:11:48 -0400

    Barra de progreso con Computed

--- computed/js/app.js
+++ computed/js/app.js
@@ -1,11 +1,20 @@
 const app = new Vue({
   el: '#app',
   data: {
-    mensaje: "Hola mundo con Vue"
+    mensaje: "Hola mundo con Vue",
+    contador: 0,
+    color: 'bg-success'
   },
   computed: {
     invertido(){
       return this.mensaje.split('').reverse().join('');
+    },
+    cambiarColor(){
+      return {
+        'bg-success' : this.contador <= 10,
+        'bg-warning' : this.contador > 10 && this.contador < 20,
+        'bg-danger' : this.contador >= 20
+      }
     }
   }
 })
